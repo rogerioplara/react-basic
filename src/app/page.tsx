cee05e4c12d421d312a7bc3d6a6f8fc9bfa3baa7
rogerioplara <rogerioo.lara@hotmail.com>
2025-07-21 11:26:03 -0300

    Página simples de exibição de imagens

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,46 @@
-import { GradeTable } from '@/components/StudentTable';
-import { students } from '@/data/students';
+"use client"
+
+import { Modal } from "@/components/Modal";
+import { PhotoItem } from "@/components/PhotoItem";
+import { photoList } from "@/data/photoList";
+import { useState } from "react";
 
 const Page = () => {
+  const [showModal, setShowModal] = useState(false);
+  const [imageOfModal, setImageOfModal] = useState('');
+
+  const openModal = (id: number) => {
+    const photo = photoList.find(item => item.id === id);
+    if (photo) {
+      setImageOfModal(photo.url);
+      setShowModal(true);
+    }
+  }
+
+  const closeModal = () => {
+    setShowModal(false);
+  }
+
   return (
-    <div className='w-screen h-screen flex flex-col justify-center items-center text-black
-    bg-gradient-to-r from-sky-500 to-indigo-500'>
-      <GradeTable 
-        list={students}
-      />
+    <div className='mx-2'>
+      <h1 className="text-center text-3xl font-bold my-10">Fotos Intergaláticas</h1>
+
+      <section className="container max-w-5xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        {photoList.map(item => (
+          <PhotoItem
+            key={item.id}
+            photo={item}
+            onClick={() => openModal(item.id)}
+          />
+        ))}
+      </section>
+
+      {showModal &&
+        <Modal
+          image={imageOfModal}
+          closeModal={closeModal}
+        />
+      }
     </div>
   );
 }
---
+++ src/types/Name.ts
@@ -0,0 +1,4 @@
+export type Name = {
+    name: string,
+    lastName: string,
+}
